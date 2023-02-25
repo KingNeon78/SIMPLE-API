9cb14a864510b07ead5a91d8a3713e4f30e3f8da
KingNeon78 <111181442+KingNeon78@users.noreply.github.com>
2023-02-25 13:04:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+// const http = require("http");
+
+// // Create Server 
+// const server = http.createServer();
+
+// console.log(" server", server);
+
+// server.listen(3000, () => {
+//  console.log('Server is created Successfully ');
+
+// });
+
+// Get , POST , PUT , PATCH 
+// /*
+// const express = require('express')
+// const app = express()
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+
+// app.listen(3000) */
+
+//   3/
+// var http = require('http');
+
+// //create a server object:
+// http.createServer(function (req, res) {
+//   res.write('Hello World!'); //write a response to the client
+//   res.end(); //end the response
+// }).listen(8080); //the server object listens on port 8080
+
+              
